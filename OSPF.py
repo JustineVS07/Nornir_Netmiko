8237d477e852d473159bf3f677631d07b248391e
JustineVS07 <70418269+JustineVS07@users.noreply.github.com>
2025-07-22 22:38:43 +0200

    Update OSPF.py

--- OSPF.py
+++ OSPF.py
@@ -3,7 +3,7 @@ from nornir_netmiko import netmiko_send_config
 from nornir_jinja2.plugins.tasks import template_file
 from nornir_utils.plugins.functions import print_result
 
-nr = InitNornir(config_file=r"F:\NETWORKING\Automation and Programming\VSCode\Learning Python\Nornir Automation\PRACTICAS\config.yaml")
+nr = InitNornir(config_file="config.yaml")
 
 #nr = nr.filter(platform= "arista_eos")
 
